06aacab75962621ee68f6dfe419c34ce6ddf609a
Kyle Harding <kylemharding@gmail.com>
2018-03-15 08:11:14 -0400

    add timezone wrapper to node-red; add ssh service

---
+++ ssh/start.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# create ssh dir if it doesn't exist
+if [ ! -d "${HOME}/.ssh" ]
+then
+	mkdir -p "${HOME}/.ssh"
+fi
+
+# generate ssh key if one does not exist
+if [ ! -f "${HOME}/.ssh/id_rsa" ]
+then
+	ssh-keygen -q -t "rsa" -N '' -f "${HOME}/.ssh/id_rsa" -C "$(id -un)@$(hostname) $(date)"
+fi
+
+# set permissions on ssh dir
+chown -R root:root "${HOME}/.ssh"
+chmod -R 700 "${HOME}/.ssh"
+
+# generate ssh host keys
+/usr/bin/ssh-keygen -A
+
+# start ssh service
+rc-service ssh start
