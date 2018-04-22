ce952e1e75ea18b17b88ce18eb21a9a8b501988a
Kyle Harding <kylemharding@gmail.com>
2018-04-22 17:18:03 +0000

    rename some volumes

--- huesensors2mqtt/start.sh
+++ huesensors2mqtt/start.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-if [ ! -e /data/config.json ]
+mkdir -p /root/.hue-mqtt-bridge 2>/dev/null || true
+
+if [ ! -e /root/.hue-mqtt-bridge/config.json ]
 then
-	cp -a ./config.sample.json /data/config.json
+	cp -a ./config.sample.json /root/.hue-mqtt-bridge/config.json
 fi
 
 if [ ! -e ./config.json ]
 then
-	ln -s /data/config.json ./config.json
+	ln -s /root/.hue-mqtt-bridge/config.json ./config.json
 fi
 
 npm start
