e7f6d5a9ec7c60a6f44ddd335e665aa3971c7589
Kyle Harding <kylemharding@gmail.com>
2018-04-23 16:33:23 +0000

    avoid persistent volume for hue2mqtt; use env config instead

--- huesensors2mqtt/start.sh
+++ huesensors2mqtt/start.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
-mkdir -p /root/.hue-mqtt-bridge 2>/dev/null || true
-
-if [ ! -e /root/.hue-mqtt-bridge/config.json ]
-then
-	cp -a ./config.sample.json /root/.hue-mqtt-bridge/config.json
-fi
-
-if [ ! -e ./config.json ]
-then
-	ln -s /root/.hue-mqtt-bridge/config.json ./config.json
-fi
+cat >./config.json <<EOL
+{
+  "broker": {
+    "host": "${BROKER_IP:-localhost}",
+    "username": "${BROKER_USER:-}",
+    "password": "${BROKER_PASS:-}"
+  },
+  "bridges": [
+    {
+      "host": "${BRIDGE_IP:-}",
+      "username": "${BRIDGE_USER:-}",
+      "interval": ${BRIDGE_INT:-500},
+      "prefix": "${BRIDGE_PRE:-hue}"
+    }
+  ]
+}
+EOL
 
 npm start
 
