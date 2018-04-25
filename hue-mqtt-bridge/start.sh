245f242cf7257c4b284ceedb73018600cb603652
Kyle Harding <kylemharding@gmail.com>
2018-04-25 13:22:44 +0000

    change some variable names

--- hue-mqtt-bridge/start.sh
+++ hue-mqtt-bridge/start.sh
@@ -3,15 +3,15 @@
 cat >./config.json <<EOL
 {
   "broker": {
-    "host": "${BROKER_IP:-localhost}",
-    "username": "${BROKER_USER:-}",
-    "password": "${BROKER_PASS:-}"
+    "host": "${MQTT_IP:-localhost}",
+    "username": "${MQTT_USERNAME:-}",
+    "password": "${MQTT_PASSWORD:-}"
   },
   "bridges": [
     {
       "host": "${BRIDGE_IP:-}",
       "username": "${BRIDGE_USER:-}",
-      "interval": ${POLL_INTERVAL:-500},
+      "interval": ${POLLING_INTERVAL:-500},
       "prefix": "${MQTT_PREFIX:-hue}"
     }
   ]
