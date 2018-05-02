53f2a1d5cf5a99de1e7074840e16d2575388fd68
Kyle Harding <kylemharding@gmail.com>
2018-05-02 18:30:09 +0000

    hue2mqtt start script requires bash

--- hue2mqtt/start.sh
+++ hue2mqtt/start.sh
@@ -1,4 +1,7 @@
-#!/bin/sh
+#!/bin/bash
+
+# set timezone
+RUN ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
 
 params=" "
 [ -n "${VERBOSITY:-}" ] && params+="-v ${VERBOSITY} "
